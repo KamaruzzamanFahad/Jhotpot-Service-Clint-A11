68d2817a18fdf602e808091b8587b635c54777fa
KamaruzzamanFahad <mihacker41@gmail.com>
2024-05-10 01:29:57 +0600

    nav ber added

--- src/Components/Navber.jsx
+++ src/Components/Navber.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink, } from 'react-router-dom';
 import { AuthContext } from '../Provider/AuthProvider';
 
 
@@ -48,15 +48,15 @@ const Navber = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </div>
                         <ul style={mebustyl} tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><Link to={'/'}>Home</Link></li>
-                            <li><Link to={'/'}>Services</Link></li>
+                            <li><NavLink to={'/'}>Home</NavLink></li>
+                            <li><NavLink to={'/'}>Services</NavLink></li>
                             <li>
-                                <Link to={'/'}>Dashboard</Link>
+                                <NavLink to={'/'}>Dashboard</NavLink>
                                 <ul className="p-2 w-48">
-                                    <li><Link to={'/'}>Add Service</Link></li>
-                                    <li><Link to={'/'}>Manage Service</Link></li>
-                                    <li><Link to={'/'}>Booked-Services</Link></li>
-                                    <li><Link to={'/'}>Service-To-Do</Link></li>
+                                    <li><NavLink to={'/'}>Add Service</NavLink></li>
+                                    <li><NavLink to={'/'}>Manage Service</NavLink></li>
+                                    <li><NavLink to={'/'}>Booked-Services</NavLink></li>
+                                    <li><NavLink to={'/'}>Service-To-Do</NavLink></li>
                                 </ul>
                             </li>
                         </ul>
@@ -66,16 +66,16 @@ const Navber = () => {
                 <div className="navbar-center hidden lg:flex">
 
                     <ul className="menu menu-horizontal px-1">
-                        <li><Link to={'/'}>Home</Link></li>
-                        <li><Link to={'/'}>Services</Link></li>
+                        <li><NavLink to={'/'}>Home</NavLink></li>
+                        <li><NavLink to={'/'}>Services</NavLink></li>
                         <li>
                             <details>
                                 <summary>Dashboard</summary>
                                 <ul style={mebustyl} className="p-2 w-56 z-[10]">
-                                    <li><Link to={'/'}>Add Service</Link></li>
-                                    <li><Link to={'/'}>Manage Service</Link></li>
-                                    <li><Link to={'/'}>Booked-Services</Link></li>
-                                    <li><Link to={'/'}>Service-To-Do</Link></li>
+                                    <li><NavLink to={'/'}>Add Service</NavLink></li>
+                                    <li><NavLink to={'/'}>Manage Service</NavLink></li>
+                                    <li><NavLink to={'/'}>Booked-Services</NavLink></li>
+                                    <li><NavLink to={'/'}>Service-To-Do</NavLink></li>
                                 </ul>
                             </details>
                         </li>
@@ -103,7 +103,7 @@ const Navber = () => {
                             </div>
                         </>
                             : <div className='flex justify-end items-center flex-row'>
-                                <Link to={'/login'}><button className="btn">Log In</button></Link>
+                                <NavLink to={'/login'}><button className="btn">Log In</button></NavLink>
                             </div>
 
 
